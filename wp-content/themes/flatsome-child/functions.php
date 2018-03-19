bf2eff5350d1be1ee64cafadd1cefa7991b5b40b
qky <qky1412@gmail.com>
2018-03-19 11:16:42 +0800

    增加了admin-menu-editor和custom-post-type-ui插件；初步搭建了teacher-loop

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -6,12 +6,6 @@ function annointed_admin_bar_remove() {
 }
 add_action('wp_before_admin_bar_render', 'annointed_admin_bar_remove', 0);
 
-// function excerpt_read_more_link($output) {
-//     global $post;
-//     $output = mb_substr($output,0, 200);
-//     return $output . '<span><a href="'. get_permalink($post->ID).'">阅读全文...</a></span>';
-//     }
-//    add_filter('the_excerpt', 'excerpt_read_more_link');
 function dez_filter_chinese_excerpt( $output ) {
     global $post;
     //check if its chinese character input
@@ -22,4 +16,53 @@ function dez_filter_chinese_excerpt( $output ) {
     return $output;
     }
     add_filter( 'get_the_excerpt', 'dez_filter_chinese_excerpt' );
+
+    function cptui_register_my_cpts_teacher() {
+
+        /**
+         * Post Type: Teachers.
+         */
+    
+        $labels = array(
+            "name" => __( "Teachers", "" ),
+            "singular_name" => __( "Teacher", "" ),
+            "menu_name" => __( "Teacher", "" ),
+            "all_items" => __( "All Teachers", "" ),
+            "add_new" => __( "Add New", "" ),
+            "add_new_item" => __( "Add New Teacher", "" ),
+            "edit_item" => __( "Edit Teacher", "" ),
+            "new_item" => __( "New Teacher", "" ),
+            "view_item" => __( "View Teacher", "" ),
+            "view_items" => __( "View Teachers", "" ),
+            "search_items" => __( "Search Teacher", "" ),
+            "not_found" => __( "No Teachers found", "" ),
+        );
+    
+        $args = array(
+            "label" => __( "Teachers", "" ),
+            "labels" => $labels,
+            "description" => "",
+            "public" => true,
+            "publicly_queryable" => true,
+            "show_ui" => true,
+            "show_in_rest" => false,
+            "rest_base" => "",
+            "has_archive" => false,
+            "show_in_menu" => true,
+            "exclude_from_search" => false,
+            "capability_type" => "post",
+            "map_meta_cap" => true,
+            "hierarchical" => false,
+            "rewrite" => array( "slug" => "teacher", "with_front" => true ),
+            "query_var" => true,
+            "supports" => array( "title", "editor", "thumbnail" ),
+        );
+    
+        register_post_type( "teacher", $args );
+    }
+    
+    add_action( 'init', 'cptui_register_my_cpts_teacher' );
+    add_action( 'init', function () {
+        add_ux_builder_post_type( 'teacher' );
+    } );
 ?>
---
+++ wp-content/themes/flatsome-child/posts-templates/teacher-loop.php
@@ -0,0 +1,22 @@
+<div class="su-posts su-posts-teaser-loop">
+	<?php
+		// Posts are found
+		if ( $posts->have_posts() ) {
+			while ( $posts->have_posts() ) :
+				$posts->the_post();
+				global $post;
+				?>
+				<div id="su-post-<?php the_ID(); ?>" class="su-post">
+					<img src="<?php the_field('avatar'); ?>" alt="" width="320" height="180"/>
+					<h2 class="su-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<h1><?php the_field('gender'); ?></h1>
+				</div>
+				<?php
+			endwhile;
+		}
+		// Posts not found
+		else {
+			echo '<h4>' . __( 'Posts not found', 'shortcodes-ultimate' ) . '</h4>';
+		}
+	?>
+</div>
